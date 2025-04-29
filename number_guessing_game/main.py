2826f57cf405ae28e7c36789c03ac1835f792b44
ShaziaNasir <shazianasir303@gmail.com>
2025-04-29 14:34:13 +0500

    adding file

---
+++ number_guessing_game/main.py
@@ -0,0 +1,24 @@
+import random
+print("Welcome to the game,this is a number guessing game! \nyou got 5 attempts to guess the number between 50 and 100 lets start the game!")
+
+number_to_guess=random.randint(50,100)
+
+chances=5
+
+guess_counter=0
+
+while guess_counter<chances:
+    guess_counter=guess_counter+1
+    my_guess= int(input("Enter your guess"))
+
+    if my_guess ==number_to_guess:
+        print(f"The number is{number_to_guess} and you found it right!!{guess_counter}")
+        break
+    elif guess_counter>=chances and my_guess!=number_to_guess:
+        print(f"Oops sorry,the number is{number_to_guess}better luck next time!")
+
+    elif my_guess< number_to_guess:
+        print(f"Your guess is very low,try again!")   
+
+    elif my_guess> number_to_guess:
+        print(f"Your guess is very high,try again!")       
